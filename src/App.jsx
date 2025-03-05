b3d0a9a6efbbf62149d49dbf8d3a38ec92876c70
Fahim047 <fahimulislam58@gmail.com>
2025-03-05 23:41:38 +0600

    ➕ add shadcn component library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
-	return <div className="text-3xl text-red-500">Hello</div>;
+	return (
+		<div className="text-3xl text-red-500">
+			<Button>Click me</Button>
+		</div>
+	);
 };
 
 export default App;
